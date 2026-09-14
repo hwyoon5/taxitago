006a18fdfee2767220670d05cc27b3979b218d94
hwyoon5 <hwyoon5@gmail.com>
2026-09-14 15:52:52 +0900

    fix: clear build cache and update gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-/** @type {import('next').Next.jsConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true, // 모든 하위 경로를 폴더 형식으로 빌드하여 404 방지
+  trailingSlash: true,
 };
-
 export default nextConfig;
